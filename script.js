fbfe064781e3a19fc4d60b005e24001e1c2a9289
samiaul <samiaul.56@gmail.com>
2024-09-27 12:17:10 +0200

    Fixed sidebar animation and added contact animation

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+/*Menu Radiobuttons*/
+
 const input_studies = document.getElementById('input-studies');
 const input_projects = document.getElementById('input-projects');
 const input_experiences = document.getElementById('input-experiences');
@@ -9,38 +11,45 @@ const section_projects = document.getElementById('projects');
 const section_experiences = document.getElementById('experiences');
 const section_other = document.getElementById('other');
 
+function resetButtons() {
+    section_studies.style.display = 'none';
+    section_projects.style.display = 'none';
+    section_experiences.style.display = 'none';
+    section_other.style.display = 'none';
+}
+
 input_studies.addEventListener('change', function () {
-    if (input_studies.checked) {
-        section_studies.style.display = 'block';
-        section_projects.style.display = 'none';
-        section_experiences.style.display = 'none';
-        section_other.style.display = 'none';
-    }
-});
+    if (input_studies.checked) {resetButtons(); section_studies.style.display = 'block'}});
 
 input_projects.addEventListener('change', function () {
-    if (input_projects.checked) {
-        section_studies.style.display = 'none';
-        section_projects.style.display = 'block';
-        section_experiences.style.display = 'none';
-        section_other.style.display = 'none';
-    }
-});
+    if (input_projects.checked) {resetButtons(); section_projects.style.display = 'block'}});
 
 input_experiences.addEventListener('change', function () {
-    if (input_experiences.checked) {
-        section_studies.style.display = 'none';
-        section_projects.style.display = 'none';
-        section_experiences.style.display = 'block';
-        section_other.style.display = 'none';
-    }
-});
+    if (input_experiences.checked) {resetButtons(); section_experiences.style.display = 'block';}});
 
 input_other.addEventListener('change', function () {
-    if (input_other.checked) {
-        section_studies.style.display = 'none';
-        section_projects.style.display = 'none';
-        section_experiences.style.display = 'none';
-        section_other.style.display = 'block';
-    }
-});
+    if (input_other.checked) {resetButtons(); section_other.style.display = 'block'}});
+
+
+/*Sidebar Show/Hide animation*/
+
+const sidebar_button = document.getElementById('sidebar-button-input');
+const sidebar_show = document.getElementById('show');
+const sidebar_hide = document.getElementById('hide');
+const sidebar = document.getElementById('sidebar');
+
+function toggleAnimation(reverse= false) {
+
+    sidebar.classList.remove('animated', 'reversed');
+
+    sidebar_hide.style.display = 'none';
+    sidebar_show.style.display = 'none';
+
+    void sidebar.offsetWidth;
+
+    if (reverse) {sidebar.classList.add('reversed'); sidebar_hide.style.display = 'block'}
+    else {sidebar.classList.add('animated'); sidebar_show.style.display = 'block'}
+}
+
+sidebar_button.addEventListener('change', function () {
+    toggleAnimation(sidebar_button.checked)});
